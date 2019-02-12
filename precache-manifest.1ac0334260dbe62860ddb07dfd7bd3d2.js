b73f94bc16ee6b0fde2ef7627c3e3a0a8996b092
David Starr <dstarrtrey@gmail.com>
2019-02-11 16:08:27 -0800

    Updates

---
+++ precache-manifest.1ac0334260dbe62860ddb07dfd7bd3d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c910f85d345057439ab1",
+    "url": "/Markdown-Previewer/static/css/main.64e3b801.chunk.css"
+  },
+  {
+    "revision": "c910f85d345057439ab1",
+    "url": "/Markdown-Previewer/static/js/main.c910f85d.chunk.js"
+  },
+  {
+    "revision": "1710ee809fc1ab1019d1",
+    "url": "/Markdown-Previewer/static/js/1.1710ee80.chunk.js"
+  },
+  {
+    "revision": "1ef1a603158ce56d01ba",
+    "url": "/Markdown-Previewer/static/js/runtime~main.1ef1a603.js"
+  },
+  {
+    "revision": "cac16690ed1701b57d271962edbb9163",
+    "url": "/Markdown-Previewer/index.html"
+  }
+];
